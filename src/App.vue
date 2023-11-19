3f3c4d15731ff1af09c5d2d4abc4277c1d1c3f13
caiocroccia22 <caiocroccia22@gmail.com>
2023-11-19 15:13:32 -0300

    Ola mundo e anotações

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
+
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--Feita a importação do Helloworld.vue é possível referênciar ela no template do Vue.js-->
+    <!--Após isso temos o atributo msg, que é criado manualmente para receber um valor-->
+    <HelloWorld msg="Olá mundo"/>
   </div>
 </template>
 
 <script>
+/*Importação do componente Hello world.vue no script*/
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
+    /*Alem da importação do componente Hello world no script, temos sua declaração aqui no components também*/
     HelloWorld
   }
 }
